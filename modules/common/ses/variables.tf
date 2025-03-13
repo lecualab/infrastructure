54c1f24ab2c68595d9c039a12dc1e0038956c2c2
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-03-13 11:18:56 -0500

    feat(common): add ses

---
+++ modules/common/ses/variables.tf
@@ -0,0 +1,20 @@
+variable "environment" {
+  description = "The environment in which the resources are created"
+  type        = string
+  default     = "production"
+}
+
+variable "region" {
+  description = "The region in which the resources are created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ses_settings" {
+  description = "The settings for the SES resources"
+  sensitive   = true
+  type = object({
+    domain_name    = string
+    email_identity = string
+  })
+}
